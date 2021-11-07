c3766106bef0e1225b7374a3fb1d07d94851382c
Scaramir <maxotto45@gmail.com>
2021-11-07 19:10:03 +0100

    standard clustering added. (modified <ctrl+c->ctrl+v>-code

--- import_mat_from_txt_as_np.py
+++ import_mat_from_txt_as_np.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 # Option 1:
 def matrix_loader(file_name):
     """
@@ -11,16 +10,57 @@ def matrix_loader(file_name):
     lines = [line.strip() for line in lines]
     lines = [line.split(' ') for line in lines]
     lines = [[x for x in line] for line in lines]
+
     return lines
 
 # Option 2:
 def matrix_loader_cheaty(file_name):
     """
-    Loads a matrix from a text file as np.
+    Loads a matrix from a text file to np.
     """
     matrix = np.loadtxt(file_name, delimiter=' ', dtype=np.chararray)
+
     return matrix
 
+matrix_a = matrix_loader('txt.txt')
+print(matrix_a)
+matrix_b = matrix_loader_cheaty('txt.txt')
+print(matrix_b)
 
-print(matrix_loader('txt.txt'))
-print(matrix_loader_cheaty('txt.txt'))
+
+# Single-linkage Clustering 
+# (Beware, not mine! This is just an example solution based on wiki git and random 3am motivation! At least I think so...; will check it tomorrow!) 
+def UPGMA(matrix):
+    """
+    UPGMA clustering algorithm.
+    """
+    # Initialize the distance matrix.
+    matrix = np.array(matrix)
+    distance_matrix = np.zeros((len(matrix), len(matrix)))
+    distance_matrix[:] = np.inf
+
+    # Set the diagonal of the distance matrix to 0.
+    for i in range(len(matrix)):
+        distance_matrix[i, i] = 0
+
+    # Iteratively update the distance matrix.
+    while len(matrix) > 2:
+        # Find the minimum distance in the distance matrix.
+        min_dist = np.min(distance_matrix)
+        min_dist_i = np.where(distance_matrix == min_dist)[0][0]
+        min_dist_j = np.where(distance_matrix == min_dist)[1][0]
+
+        # Merge the two closest clusters.
+        matrix = np.vstack((matrix[min_dist_i], matrix[min_dist_j]))
+        matrix = np.delete(matrix, [min_dist_j, min_dist_i], axis=0)
+
+        # Update the distance matrix.
+        for i in range(len(matrix)):
+            for j in range(len(matrix)):
+                if i != j:
+                    distance_matrix[i, j] = np.min(
+                        [distance_matrix[i, j],
+                         distance_matrix[i, min_dist_i] +
+                         distance_matrix[min_dist_j, j]])
+
+    return matrix
